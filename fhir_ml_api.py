5bfc1138cc26c892e7dd2d02695de01d8b67a29c
Ethelbert <nedu47@gmail.com>
2024-02-25 22:07:26 +0000

    Update fhir_ml_api.py
    Updated the API code for model deployment.

--- fhir_ml_api.py
+++ fhir_ml_api.py
@@ -16,37 +16,46 @@ model = load_model('fhir_ethnicity_classifier')
 # Define the FHIR API endpoint
 api_url = os.environ["api_url"]
 
-# Define the endpoint for making predictions
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get input data from the request
-    data = request.json
-    
-    # Convert the input data into a DataFrame
-    input_data = pd.DataFrame(data, index=[0])
-    
-    # Make predictions using the loaded model
-    predictions = predict_model(model, data=input_data)
-    
-    # Convert the prediction to FHIR-compliant format
-    prediction_value = predictions.iloc[0][-1] 
-    fhir_prediction = {
-        "resourceType": "Observation",
-        "status": "final",
-        "code": {
-            "coding": [
-                {
-                    "system": "http://loinc.org",
-                    "code": "9279-1",
-                    "display": "Prediction"
-                }
-            ]
-        },
-        "valueString": "Hispanic or Latino" if prediction_value == 'Hispanic or Latino' else "Not Hispanic or Latino"
-    }
-    
-    return jsonify(fhir_prediction)
+# Define input pydantic model
+class InputModel(BaseModel):
+    elapsed_time_days: float
+    age: float
+    gender: str
+    race: str
+    blood_leukocytes_volume: float
+    blood_erythrocyte_volume: float
+    blood_hemoglobin_volume: float
+    blood_hematocrit_volume: float
+    mcv_count: float
+    mch_count: float
+    mchc_count: float
+    erythrocyte_distribution_width_count: float
+    platelets_volume_count: float
+    platelet_mean_volume_count: float
 
+# Define an enum for prediction types
+class PredictionEnum(str, Enum):
+    Latino = "Hispanic or Latino"
+    African = "African"
+
+# Define output pydantic model with the Enum
+class OutputModel(BaseModel):
+    prediction: PredictionEnum
+
+# Define predict function
+@app.post("/predict", response_model=OutputModel)
+def predict(data: InputModel):
+    data_df = pd.DataFrame([data.dict()])
+    predictions = predict_model(model, data=data_df)
+    prediction_column = predictions.columns[14]  # Assuming the prediction column is the first column
+
+    # Transform integer prediction to string based on conditions
+    prediction_value = int(predictions[prediction_column].iloc[0])
+    prediction_str = PredictionEnum.Latino if prediction_value == 1 else PredictionEnum.African
+
+    return {"prediction": prediction_str}
+
+# Define the endpoint for sending data to the FHIR API
 @app.route('/send_to_fhir', methods=['POST'])
 async def send_to_fhir():
     # Get input data from the request
